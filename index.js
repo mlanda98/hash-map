407611ed1a782678caba7a0ee36035e2f94a4b87
Mayra Landa <mlandae16@gmail.com>
2024-05-30 16:23:15 -0700

    add contains(key) function

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
 class HashTable {
-  constructor(size = 100){
+  constructor(size = 100) {
     this.buckets = new Array(size);
     this.size = size;
   }
 
   hash(key) {
-   let hashCode = 0;
-   const primeNumber = 31;
-   for (let i = 0; i < key.length; i++) {
-     hashCode = primeNumber * hashCode + key.charCodeAt(i);
-   }
-
-   return hashCode % this.size;
- } 
-
- insert(key, value){
-  const index = this.hash(key);
-  if (!this.buckets[index]){
-    this.buckets[index] = [];
-  }
-  for (let pair of this.buckets[index]){
-    if (pair[0] === key){
-      pair[1] = value;
-      return;
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
-  }
-  this.buckets[index].push([key, value]);
- } 
 
- get(key){
-  const index = this.hash(key);
-  if (!this.buckets[index]){
+    return hashCode % this.size;
+  }
+
+  insert(key, value) {
+    const index = this.hash(key);
+    if (!this.buckets[index]) {
+      this.buckets[index] = [];
+    }
+    for (let pair of this.buckets[index]) {
+      if (pair[0] === key) {
+        pair[1] = value;
+        return;
+      }
+    }
+    this.buckets[index].push([key, value]);
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    if (!this.buckets[index]) {
+      return null;
+    }
+    for (let pair of this.buckets[index]) {
+      if (pair[0] === key) {
+        return pair[1];
+      }
+    }
     return null;
-  } 
-  for (let pair of this.buckets[index]){
-    if (pair[0] === key){
-      return pair[1];
+  }
+  contains(key) {
+    if (this.get(key) !== null){
+      return true
+    } else{
+      return false;
     }
   }
-  return null;
- }
 }
+
+
 
 const HashTable = new HashTable();
 
